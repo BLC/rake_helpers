ce6384bfdd7e6f4ef1f727587bf0b97e92775ac3
Richie Vos <jerry.vos@gmail.com>
2009-07-08 17:42:17 -0500

    Gem'ified this code.

---
+++ lib/rake_helpers.rb
@@ -0,0 +1,9 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+require 'rake_helpers/config'
+require 'rake_helpers/erb_helper'
+
+module RakeHelpers
+  VERSION = '0.0.1'
+end
