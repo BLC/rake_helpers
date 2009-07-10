86631691c0bf1351f824d473e7d5d9fc93a33990
Richie Vos <jerry.vos@gmail.com>
2009-07-10 10:38:41 -0500

    No longer using ActiveSupport so need to require Singleton.

--- lib/rake_helpers.rb
+++ lib/rake_helpers.rb
@@ -5,5 +5,5 @@ require 'rake_helpers/config'
 require 'rake_helpers/erb_helper'
 
 module RakeHelpers
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
 end
--- lib/rake_helpers/config.rb
+++ lib/rake_helpers/config.rb
@@ -1,3 +1,5 @@
+require 'singleton'
+
 module MP
   class Config
     include Singleton
